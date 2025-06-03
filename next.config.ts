9eab0b02890512a00e1c500592cf01b694b54545
AhmadTaji <ahmadtajiafg@gmail.com>
2025-06-03 09:41:01 +0430

    Initial commit from Create Next App

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+// module.exports = {
+//   images: {
+//     domains: ['images.unsplash.com', 'cdn.pixabay.com'], // add any image hosts you use
+//   },
+// };
+module.exports = {
+  images: {
+    domains: ['images.unsplash.com', 'cdn.pixabay.com'],
+  },
+};
